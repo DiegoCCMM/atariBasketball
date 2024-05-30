5459dc5fd5d01c8982103b042369277b5e36e80f
Diego Caballé <diegocaballe@hotmail.com>
2024-05-30 18:57:23 +0200

    default godot physics movement for player

--- scripts/movement_component.gd
+++ scripts/movement_component.gd
@@ -2,22 +2,28 @@ extends Node
 
 class_name MovementComponent
 
-@export var speed: float = 200
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
 
-func get_velocity() -> Vector2:
-	var velocity = Vector2.ZERO
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
-	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
 
-	return velocity.normalized() * speed
+func move(node, delta):
+	# Add the gravity.
+	if not get_parent().is_on_floor():
+		get_parent().velocity.y += gravity * delta
 
-func move(entity: Node2D, delta: float):
-	var velocity = get_velocity()
-	entity.position += velocity * delta
+	# Handle jump.
+	if Input.is_action_just_pressed("move_up") and get_parent().is_on_floor():
+		get_parent().velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("move_left", "move_right")
+	if direction:
+		get_parent().velocity.x = direction * SPEED
+	else:
+		get_parent().velocity.x = move_toward(get_parent().velocity.x, 0, SPEED)
+
+	get_parent().move_and_slide()
